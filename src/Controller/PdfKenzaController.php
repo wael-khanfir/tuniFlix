a4c40cd9e23b80336f5a18f89cacd48080013eea
kenza <bendebba.cyrinekenza@esprit.tn>
2021-05-20 15:57:00 +0100

    web (bundle notification + api map+bundle swiftmailer+bundle qr code+JSON)

---
+++ src/Controller/PdfKenzaController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CinemaRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
+class PdfKenzaController extends AbstractController
+{
+    /**
+     * @Route("/pdf/kenza", name="pdf_kenza")
+     */
+    public function index(CinemaRepository $CinemaRepository)
+    {
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('cinema/PdfCinema.html.twig', [
+            'cinemas' => $CinemaRepository->findAll(),
+        ]);
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (force download)
+        $dompdf->stream("mm.pdf", [
+            "Attachment" => true
+        ]);
+    }
+}
+
